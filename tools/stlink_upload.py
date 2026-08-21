95ce35477665a10a14fb36b579990bc36ff3f38c
Finn <finnyoun9@gmail.com>
2026-08-21 12:53:04 +0800

    fix(ci): load OpenOCD only for upload

--- tools/stlink_upload.py
+++ tools/stlink_upload.py
@@ -10,35 +10,34 @@ from os.path import join
 
 Import("env")
 
-FLASH_ADDRESS = {
-    "bluepill": "0x08000000",
-    "app": "0x08002000",
-}[env.subst("$PIOENV")]
+if "upload" in COMMAND_LINE_TARGETS:
+    FLASH_ADDRESS = {
+        "bluepill": "0x08000000",
+        "app": "0x08002000",
+    }[env.subst("$PIOENV")]
 
-platform = env.PioPlatform()
-openocd_dir = platform.get_package_dir("tool-openocd")
-openocd_bin = "openocd.exe" if sys.platform == "win32" else "openocd"
-openocd = join(openocd_dir, "bin", openocd_bin)
-scripts_dir = join(openocd_dir, "openocd", "scripts")
-interface_cfg = join(scripts_dir, "interface", "stlink.cfg")
-target_cfg = join(scripts_dir, "target", "stm32f1x.cfg")
+    platform = env.PioPlatform()
+    openocd_dir = platform.get_package_dir("tool-openocd")
+    openocd_bin = "openocd.exe" if sys.platform == "win32" else "openocd"
+    openocd = join(openocd_dir, "bin", openocd_bin)
+    scripts_dir = join(openocd_dir, "openocd", "scripts")
+    interface_cfg = join(scripts_dir, "interface", "stlink.cfg")
+    target_cfg = join(scripts_dir, "target", "stm32f1x.cfg")
 
+    def _upload_source_path(env, source):
+        """OpenOCD Tcl requires forward slashes in Windows file paths."""
+        return source.get_abspath().replace("\\", "/")
 
-def _upload_source_path(env, source):
-    """OpenOCD Tcl requires forward slashes in Windows file paths."""
-    return source.get_abspath().replace("\\", "/")
-
-
-env.Replace(
-    __stlink_upload_source_path=_upload_source_path,
-    UPLOADCMD=(
-        '"{openocd}" -f "{interface}" -f "{target}" '
-        '-c "program ${{__stlink_upload_source_path(__env__, SOURCE)}} '
-        '{address} verify reset exit"'
-    ).format(
-        openocd=openocd,
-        interface=interface_cfg,
-        target=target_cfg,
-        address=FLASH_ADDRESS,
+    env.Replace(
+        __stlink_upload_source_path=_upload_source_path,
+        UPLOADCMD=(
+            '"{openocd}" -f "{interface}" -f "{target}" '
+            '-c "program ${{__stlink_upload_source_path(__env__, SOURCE)}} '
+            '{address} verify reset exit"'
+        ).format(
+            openocd=openocd,
+            interface=interface_cfg,
+            target=target_cfg,
+            address=FLASH_ADDRESS,
+        )
     )
-)
